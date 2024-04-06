f45045c52c68536db762a04e96ffcb8b41c146e5
Abrar Ahmed Shahok <abrarahmedshahok12@gmail.com>
2024-04-06 06:52:44 +0500

    Updated Router

--- lib/routes/app_router.gr.dart
+++ lib/routes/app_router.gr.dart
@@ -9,6 +9,7 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:auto_route/auto_route.dart' as _i3;
+import 'package:flutter/material.dart' as _i4;
 import 'package:personal_notes_app/presentation/screens/add_note_screen.dart'
     as _i1;
 import 'package:personal_notes_app/presentation/screens/notes_screen.dart'
@@ -20,9 +21,14 @@ abstract class $AppRouter extends _i3.RootStackRouter {
   @override
   final Map<String, _i3.PageFactory> pagesMap = {
     AddNoteRoute.name: (routeData) {
+      final args = routeData.argsAs<AddNoteRouteArgs>(
+          orElse: () => const AddNoteRouteArgs());
       return _i3.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i1.AddNoteScreen(),
+        child: _i1.AddNoteScreen(
+          key: args.key,
+          noteId: args.noteId,
+        ),
       );
     },
     NotesRoute.name: (routeData) {
@@ -36,16 +42,40 @@ abstract class $AppRouter extends _i3.RootStackRouter {
 
 /// generated route for
 /// [_i1.AddNoteScreen]
-class AddNoteRoute extends _i3.PageRouteInfo<void> {
-  const AddNoteRoute({List<_i3.PageRouteInfo>? children})
-      : super(
+class AddNoteRoute extends _i3.PageRouteInfo<AddNoteRouteArgs> {
+  AddNoteRoute({
+    _i4.Key? key,
+    String? noteId,
+    List<_i3.PageRouteInfo>? children,
+  }) : super(
           AddNoteRoute.name,
+          args: AddNoteRouteArgs(
+            key: key,
+            noteId: noteId,
+          ),
           initialChildren: children,
         );
 
   static const String name = 'AddNoteRoute';
 
-  static const _i3.PageInfo<void> page = _i3.PageInfo<void>(name);
+  static const _i3.PageInfo<AddNoteRouteArgs> page =
+      _i3.PageInfo<AddNoteRouteArgs>(name);
+}
+
+class AddNoteRouteArgs {
+  const AddNoteRouteArgs({
+    this.key,
+    this.noteId,
+  });
+
+  final _i4.Key? key;
+
+  final String? noteId;
+
+  @override
+  String toString() {
+    return 'AddNoteRouteArgs{key: $key, noteId: $noteId}';
+  }
 }
 
 /// generated route for
